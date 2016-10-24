b83a5e6fdf4fc82f566fbbe1ed36c2f916b3dc0b
Amanda Hinchey <email": null>
2016-10-24 15:19:50 -0400

    Initial

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,44 @@
+class JobsController < ApplicationController
+
+  before_action :find_job, only: [:edit, :update, :destroy]
+
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    Job.create(job_params)
+    redirect_to jobs_path
+  end
+
+  def edit
+    # @job = Job.find(params[:id])
+  end
+
+  def update
+    # @job = Job.find(params[:id])
+    @job.update_attributes(job_params)
+    redirect_to jobs_path
+  end
+
+  def destroy
+    # @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
+  def find_job
+    @job = Job.find(params[:id])
+  end
+
+end
